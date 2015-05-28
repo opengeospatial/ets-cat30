834c42be9aa5560af2ff31df982f2c7c9bf36c6d
R. Martell <rjmartell@computer.org>
2015-05-28 14:43:29 -0700

    If ows:DefaultValue is absent check first ows:Value for status.

--- src/main/java/org/opengis/cite/cat30/opensearch/OpenSearchPreconditions.java
+++ src/main/java/org/opengis/cite/cat30/opensearch/OpenSearchPreconditions.java
@@ -1,7 +1,5 @@
 package org.opengis.cite.cat30.opensearch;
 
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.xpath.XPathConstants;
 import org.opengis.cite.cat30.CAT3;
 import org.opengis.cite.cat30.SuiteAttribute;
 import org.testng.ITestContext;
@@ -13,6 +11,7 @@ import org.opengis.cite.cat30.ErrorMessageKeys;
 import org.opengis.cite.cat30.util.CSWClient;
 import org.opengis.cite.cat30.util.XMLUtils;
 import org.testng.annotations.BeforeTest;
+import org.w3c.dom.NodeList;
 
 /**
  * Checks that various preconditions are satisfied before the tests for
@@ -24,15 +23,17 @@ public class OpenSearchPreconditions {
     /**
      * Checks that the service capabilities document advertises OpenSearch
      * support. The implementation status of the corresponding conformance class
-     * must be set to "TRUE".
+     * must be set to "TRUE". The ows:DefaultValue element takes precedence; if
+     * it does not appear then the value of the first occurrence of the
+     * ows:Value element will be checked.
      *
      * <pre>{@literal
      *<OperationsMetadata xmlns="http://www.opengis.net/ows/2.0">
      *  <!-- Operation elements omitted -->
      *  <Constraint name="http://www.opengis.net/spec/csw/3.0/conf/OpenSearch">
      *    <AllowedValues>
-     *      <Value>FALSE</Value>
      *      <Value>TRUE</Value>
+     *      <Value>FALSE</Value>
      *    </AllowedValues>
      *    <DefaultValue>TRUE</DefaultValue>
      *    <Meaning>Conformance class</Meaning>
@@ -41,11 +42,13 @@ public class OpenSearchPreconditions {
      *}
      * </pre>
      *
+     * <p>
      * An attempt will be made to retrieve an OpenSearch description document
      * from the IUT. A representation is expected to be obtained by
      * dereferencing the URI corresponding to the GetCapabilities (GET)
      * endpoint. The resulting Document object is stored as the value of the
      * suite attribute {@link SuiteAttribute#OPENSEARCH_DESCR}.
+     * </p>
      *
      * @param testContext Information about the current test run.
      *
@@ -56,18 +59,22 @@ public class OpenSearchPreconditions {
         Document cswCapabilities = (Document) testContext.getSuite().getAttribute(
                 SuiteAttribute.TEST_SUBJECT.getName());
         String xpath = String.format(
-                "//ows:Constraint[contains(@name,'%s')]/ows:DefaultValue = 'TRUE'",
+                "//ows:Constraint[contains(@name,'%s')]/ows:DefaultValue",
                 CAT3.CC_OPEN_SEARCH);
-        Boolean isSupported = null;
+        NodeList result = null;
         try {
-            isSupported = (Boolean) XMLUtils.evaluateXPath(
-                    new DOMSource(cswCapabilities), xpath, null,
-                    XPathConstants.BOOLEAN);
-        } catch (XPathExpressionException ex) { // ignore--expression ok
+            result = XMLUtils.evaluateXPath(cswCapabilities, xpath, null);
+            if (result.getLength() == 0) {
+                xpath = String.format(
+                        "//ows:Constraint[contains(@name,'%s')]//ows:Value[1]",
+                        CAT3.CC_OPEN_SEARCH);
+                result = XMLUtils.evaluateXPath(cswCapabilities, xpath, null);
+            }
+        } catch (XPathExpressionException ex) { // ignore--expressions ok
         }
-        if (!isSupported) {
-            throw new AssertionError(
-                    "OpenSearch not supported. Expected: \n" + xpath);
+        if (result.getLength() == 0
+                || !result.item(0).getTextContent().trim().equalsIgnoreCase("TRUE")) {
+            throw new AssertionError("OpenSearch not a supported capability.");
         }
         CSWClient cswClient = new CSWClient();
         cswClient.setServiceDescription(cswCapabilities);
