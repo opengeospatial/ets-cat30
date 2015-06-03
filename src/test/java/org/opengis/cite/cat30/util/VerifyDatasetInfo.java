302fbb65cece78f3b5a3cd3624ad024dcb361451
R. Martell <rjmartell@computer.org>
2015-06-03 15:37:27 -0700

    Update release notes.

--- src/test/java/org/opengis/cite/cat30/util/VerifyDatasetInfo.java
+++ src/test/java/org/opengis/cite/cat30/util/VerifyDatasetInfo.java
@@ -56,4 +56,17 @@ public class VerifyDatasetInfo {
         assertEquals("Unexpected value for last id.",
                 "urn:uuid:b1254954-f765-11e1-bf69-aa0000ae6bfc", lastId);
     }
+
+    @Test
+    public void calculateExtentUsingHttpCRSReferences() throws URISyntaxException {
+        URL url = getClass().getResource("/rsp/GetRecordsResponse-3.xml");
+        File dataFile = new File(url.toURI());
+        DatasetInfo dataset = new DatasetInfo(dataFile);
+        Envelope env = dataset.getGeographicExtent();
+        assertEquals("Unexpected CRS code", "WGS 84", env.getCoordinateReferenceSystem().getName().getCode());
+        assertArrayEquals("Unexpected coords for upper corner.",
+                new double[]{68.41, 17.92},
+                env.getUpperCorner().getCoordinate(),
+                0.005);
+    }
 }
